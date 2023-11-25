35e7d4f78e4c35a4e750badbf1078ade030ad9fc
algo <algo@gmail.com>
2023-11-25 10:21:36 -0600

    mevengoelsprite

--- sprites.py
+++ sprites.py
@@ -1,1 +1,14 @@
 import pygames
+
+class Jugador(pygame.sprite.Sprite)
+
+def __init__(self):
+	
+	super().__init__()
+
+	self.image = pygame.Surface((200, 200))
+	self.image.fill(H_FA2F2F)
+	
+	self.rect = self.image.get_rect()
+	
+	self.rect.center = (ANCHO // 2, ALTO // 2)
